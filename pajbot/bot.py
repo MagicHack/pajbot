939a77dda4d95e724e3f42488c69222c39be0481
Émile Larose-Levac <emile.larose@gmail.com>
2021-05-25 16:22:04 -0400

    NaM2

--- pajbot/bot.py
+++ pajbot/bot.py
@@ -826,10 +826,10 @@ class Bot:
 
         if self.streamer == "pepto__bismol":
             if "sunephef" in login:
-                self.timeout_login(login, 1, reason="sunephef NaM")
+                self.timeout_login(login, 1, reason="NaM silence weeb")
                 return True
             if "sunwithnofaceclap" in login:
-                self.timeout_login(login, 1, reason="sunephef NaM")
+                self.timeout_login(login, 1, reason="NaM silence weeb")
                 return True
 
         with DBManager.create_session_scope(expire_on_commit=False) as db_session:
